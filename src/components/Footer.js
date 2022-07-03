bcd3fc5d534ed6cc9bdf36d77ed58c7eea65883e
hachi-ops <elen20@protonmail.com>
2022-07-03 01:39:32 +0100

    styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,55 +1,39 @@
 import React from "react";
 
-import {
-  Box,
-  Container,
-  Row,
-  Column,
-  FooterLink,
-  Heading,
-} from "./FooterStyles.js";
+import { Container, Column, FooterLink, Heading } from "./FooterStyles.js";
 
 const Footer = () => {
   return (
-    <Box>
-      <Container>
-        <Row>
-          <Column>
-            <Heading>About Us</Heading>
-            <FooterLink href="#">Aim</FooterLink>
-            <FooterLink href="#">Vision</FooterLink>
-            <FooterLink href="#">Testimonials</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Contact Us</Heading>
-            <FooterLink href="#">some text</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Social Media</Heading>
-            <FooterLink href="#">
-              <i className="fab fa-facebook-f">
-                <span style={{ marginLeft: "10px" }}>Facebook</span>
-              </i>
-            </FooterLink>
-            <FooterLink href="#">
-              <i className="fab fa-instagram">
-                <span style={{ marginLeft: "10px" }}>Instagram</span>
-              </i>
-            </FooterLink>
-            <FooterLink href="#">
-              <i className="fab fa-twitter">
-                <span style={{ marginLeft: "10px" }}>Twitter</span>
-              </i>
-            </FooterLink>
-            <FooterLink href="#">
-              <i className="fab fa-youtube">
-                <span style={{ marginLeft: "10px" }}>Youtube</span>
-              </i>
-            </FooterLink>
-          </Column>
-        </Row>
-      </Container>
-    </Box>
+    <Container>
+      <Column>
+        <Heading>About Us</Heading>
+        <FooterLink href="#">Aim</FooterLink>
+        <FooterLink href="#">Vision</FooterLink>
+        <FooterLink href="#">Testimonials</FooterLink>
+      </Column>
+      <Column>
+        <Heading>Contact Us</Heading>
+        <FooterLink href="#">some text</FooterLink>
+      </Column>
+      <Column>
+        <Heading>Social Media</Heading>
+        <FooterLink href="#">
+          <i className="fab fa-facebook-f">
+            <span style={{ marginLeft: "10px" }}>Facebook</span>
+          </i>
+        </FooterLink>
+        <FooterLink href="#">
+          <i className="fab fa-instagram">
+            <span style={{ marginLeft: "10px" }}>Instagram</span>
+          </i>
+        </FooterLink>
+        <FooterLink href="#">
+          <i className="fab fa-twitter">
+            <span style={{ marginLeft: "10px" }}>Twitter</span>
+          </i>
+        </FooterLink>
+      </Column>
+    </Container>
   );
 };
 export default Footer;
--- src/components/FooterStyles.js
+++ src/components/FooterStyles.js
@@ -1,24 +1,11 @@
 import styled from "styled-components";
 
-export const Box = styled.div`
-  padding: 80px 60px;
-  background: white;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-
-  //   @media (max-width: 1000px) {
-  //     padding: 70px 30px;
-  //   }
-`;
-
 export const Container = styled.div`
+  position: relative;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
-  max-width: 1000px;
   margin: 0 auto;
-  /* background: red; */
 `;
 
 export const Column = styled.div`
@@ -26,16 +13,6 @@ export const Column = styled.div`
   flex-direction: column;
   text-align: left;
   margin-left: 60px;
-`;
-
-export const Row = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(185px, 1fr));
-  grid-gap: 20px;
-
-  //   @media (max-width: 1000px) {
-  //     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  //   }
 `;
 
 export const FooterLink = styled.a`
